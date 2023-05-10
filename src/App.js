1cecb943e641e4e69ee2d3575b382d9b40d2c9ba
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-10 17:59:52 +0530

    hero and news letter component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Navbar from "./compopnents/Navbar";
+import React from 'react';
+import Analytics from './components/Analytics';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
